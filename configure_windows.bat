5778f76315cd008db193bcc6f36272cd578d6122
Hayley Spellicy-Ryan <spellicyryan.hayley@gmail.com>
2025-09-16 11:01:28 -0400

    Created .bat script for Win build without GNU

---
+++ configure_windows.bat
@@ -0,0 +1,10 @@
+@echo off
+REM USAGE: sh .\ configure_windows.sh
+
+DEL /S /F /Q "build"
+DEL  "CMakeCache.txt"
+
+cmake "-S" "." "-B" "build" "-G" "Visual Studio 17 2022" "-A" "x64" "-T" "host=x64"
+
+REM Run only this to rebuild the plugin and not all of the configuration.
+cmake "--build" "build"
--- configure_windows.sh
+++ configure_windows.sh
@@ -5,4 +5,5 @@ rm -f CMakeCache.txt
 
 cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -T host=x64
 
+# Run only this to rebuild the plugin and not all of the configuration.
 cmake --build build
