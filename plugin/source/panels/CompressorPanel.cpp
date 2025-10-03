178a890a0a3a4fc3a503651ca79291fa31f69949
AustinHills <arh032904@gmail.com>
2025-10-02 23:56:46 -0400

    Made basic design for CompressorPanel. Started integration. Testing soon hopefully

--- plugin/source/panels/CompressorPanel.cpp
+++ plugin/source/panels/CompressorPanel.cpp
@@ -93,5 +93,74 @@ CompressorPanel::CompressorPanel(AudioPluginAudioProcessor& proc) : processor(pr
 
 void CompressorPanel::resized()
 {
+    auto area = getLocalBounds();
 
+    //Title label at the top
+    compressorLabel.setBounds(area.removeFromTop(30));
+
+    modeButton.setBounds(area.removeFromTop(30).reduced(10,5));
+
+    auto dials = area.reduced(10);
+    int dialWidth = dials.getWidth()/4;
+
+    auto thresholdArea = dials.removeFromLeft(dialWidth).reduced(5);
+    auto ratioArea = dials.removeFromLeft(dialWidth).reduced(5);
+    auto attackArea = dials.removeFromLeft(dialWidth).reduced(5);
+    auto releaseArea = dials.reduced(5);
+
+    //Positioning threshold label and slider
+    thresholdLabel.setBounds(thresholdArea.removeFromTop(20));
+    thresholdSlider.setBounds(thresholdArea);
+
+    //Positioning ratio label and slider
+    ratioLabel.setBounds(ratioArea.removeFromTop(20));
+    ratioSlider.setBounds(ratioArea);
+
+    //Positioning attack label and slider
+    attackLabel.setBounds(attackArea.removeFromTop(20));
+    attackSlider.setBounds(attackArea);
+
+    //Positioning release label and slider
+    releaseLabel.setBounds(releaseArea.removeFromTop(20));
+    releaseSlider.setBounds(releaseArea);
+}
+
+void CompressorPanel::sliderValueChanged(juce::Slider* s)
+{
+    if (s == &thresholdSlider)
+        processor.compressorThresholdDb = (float)thresholdSlider.getValue();
+    else if (s == &ratioSlider)
+        processor.compressorRatio = (float)ratioSlider.getValue();
+    else if (s == &attackSlider)
+        processor.compressorAttack = (float)attackSlider.getValue();
+    else if (s == &releaseSlider)
+        processor.compressorRelease = (float)releaseSlider.getValue();
+}
+
+void CompressorPanel::buttonClicked(juce::Button* b)
+{
+    if(b == &modeButton){
+        processor.isLimiterMode = b->getToggleState();
+        if(processor.isLimiterMode == 0){
+            b->setButtonText("Simple Mode");
+        }else{
+            b->setButtonText("Limiter Mode");
+        }
+        updateSliderVisibility();
+    }
+}
+
+void CompressorPanel::updateSliderVisibility()
+{
+    ratioSlider.setVisible(!processor.isLimiterMode);
+    ratioLabel.setVisible(!processor.isLimiterMode);
+    attackSlider.setVisible(!processor.isLimiterMode);
+    attackLabel.setVisible(!processor.isLimiterMode);
+}
+
+void NoiseGatePanel::paint(juce::Graphics& g)
+{
+    g.fillAll(Colors::background);
+    //g.setColour(Colors::accent);
+    g.drawRect(getLocalBounds(), 2);
 }
