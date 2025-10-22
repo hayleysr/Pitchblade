9f81adb6a3bfbc822cca14096ec044f7d20bfef3
Hayley Spellicy-Ryan <spellicyryan.hayley@gmail.com>
2025-10-22 18:18:32 -0400

    set config to debug mode

--- configure_windows.bat
+++ configure_windows.bat
@@ -7,4 +7,5 @@ DEL  "CMakeCache.txt"
 cmake "-S" "." "-B" "build" "-G" "Visual Studio 17 2022" "-A" "x64" "-T" "host=x64"
 
 REM Run only this to rebuild the plugin and not all of the configuration.
+cmake "-B" "build" "-DCMAKE_BUILD_TYPE=Debug"
 cmake "--build" "build"
