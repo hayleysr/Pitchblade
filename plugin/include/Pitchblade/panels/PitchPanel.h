194c200de0138834bd092e4f84ea7b8d9bda9788
Hayley Spellicy-Ryan <spellicyryan.hayley@gmail.com>
2025-11-09 17:35:07 -0500

    started visualizer, crashes

--- plugin/include/Pitchblade/panels/PitchPanel.h
+++ plugin/include/Pitchblade/panels/PitchPanel.h
@@ -48,6 +48,58 @@ private:
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PitchPanel)
 };
+
+
+// Visualizer node for pitch correction
+#include "Pitchblade/ui/VisualizerPanel.h"
+#include "Pitchblade/ui/RealTimeGraphVisualizer.h"
+
+class PitchNode;
+
+class PitchVisualizer : public RealTimeGraphVisualizer, public juce::ValueTree::Listener{
+public:
+    explicit PitchVisualizer(AudioPluginAudioProcessor& proc, PitchNode& node, juce::ValueTree& state)
+        : RealTimeGraphVisualizer(proc.apvts, "note", {0.f, 5000.0f}, false, 5),
+            processor(proc),
+            pitchNode(node),
+            localState(state)
+    {
+        //Listen for changes
+        localState.addListener(this);
+
+        int initialIndex = *proc.apvts.getRawParameterValue("GLOBAL_FRAMERATE");
+
+        switch(initialIndex){
+            case 0:
+                startTimerHz(5);
+                break;
+            case 1:
+                startTimerHz(15);
+                break;
+            case 2:
+                startTimerHz(30);
+                break;
+            case 3:
+                startTimerHz(60);
+                break;
+            default:
+                startTimerHz(30);
+                break;
+        }
+    }
+
+    ~PitchVisualizer() override;
+
+    //Update the graph
+    void timerCallback() override;
+
+    void valueTreePropertyChanged(juce::ValueTree& tree, const juce::Identifier& property) override;
+private:
+    AudioPluginAudioProcessor& processor;
+    PitchNode& pitchNode;
+    juce::ValueTree localState;
+};
+
 ////////////////////////////////////////////////////////////
 //reynas changes > added dsp node defn to ui panel creation
 // pitch dsp node , processes audio + makes own panel
@@ -110,6 +162,11 @@ public:
     {
         return std::make_unique<PitchPanel>(proc, getMutableNodeState());
     }
+
+    std::unique_ptr<juce::Component> createVisualizer(AudioPluginAudioProcessor& proc) override {
+        return std::make_unique<PitchVisualizer>(proc, *this, getMutableNodeState());
+    }
+
 
     std::atomic<float>& getPitchAtomic(){
         return pitchDSP.currentOutputPitch;
