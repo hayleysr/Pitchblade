aaa5eebd3328c39455c8ba14fb844f7a9fe0a3ef
AustinHills <arh032904@gmail.com>
2025-11-26 23:54:28 -0500

    Fixed it! We can now build in release mode! Installer exe only builds in release mode btw

--- configure_windows_release.bat
+++ configure_windows_release.bat
@@ -11,7 +11,7 @@ cmake "-S" "." "-B" "build" "-G" "Visual Studio 17 2022" "-A" "x64" "-T" "host=x
 
 ECHO "Building plugin..."
 cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake "--build" "build" --target Pitchblade_All
+cmake "--build" "build" --config Release --target Pitchblade_All
 
 REM --- Check if build was successful ---
 IF %ERRORLEVEL% NEQ 0 (
